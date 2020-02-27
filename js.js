d4002d2ee9411cc437929917e7e65cc5b6f7a064
sheila1 <sheilacolorado97@gmail.com>
2020-02-27 17:13:26 +0100

    Esto es una tabla

---
+++ js.js
@@ -0,0 +1,54 @@
+
+/*resultado = 0
+for(i = 1; i<11; i++){
+    resultado +=5
+    document.write('5 x '+i+ ' = '+resultado)
+    document.write("<br>")
+}
+
+
+
+
+document.getElementById('num1').innerText = num1
+document.getElementById('num2').innerText = num2
+
+document.getElementById("resultado").innerText = (Number(num1) + Number(num2))
+
+
+vaunr num1 = prompt('num1')
+v ar  num2 = prompt('num2')*/
+const numeroIntroducidoInput = document.getElementById('numeroIntroducidoInput')
+const numeroIntentosP = document.getElementById('numeroIntentosP')
+const acierto = document.getElementById('acierto')
+const fallido = document.getElementById('fallido')
+const error = document.getElementById('error')
+
+
+const intentar = document.getElementById('intentar')
+
+
+let intentos = 0
+const numeroSecreto = 4
+let numeroIntroducido = 0
+
+intentar.addEventListener('click', function(){
+    error.style.display = 'none'
+    acierto.style.display = 'none'
+    fallido.style.display = 'none'
+    numeroIntroducido = parseInt(numeroIntroducidoInput.value)
+    intentos +=1
+    console.log('numeroIntroducido: '+numeroIntroducido)
+    console.log('numerosecreto: '+ numeroSecreto)
+    numeroIntentosP.innerHTML = intentos
+    if (numeroIntroducido < 1 || numeroIntroducido > 10){
+        error.style.display = 'flex'
+    }
+    else if(numeroIntroducido === numeroSecreto) {
+        acierto.style.display='flex'
+    }
+    else {
+        fallido.style.display = 'flex'
+    }
+})
+
+ 
